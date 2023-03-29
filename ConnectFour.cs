f67859edbf62d00bb943314b170dd1051d8779de
Travis Unruh <travis.unruh@gmail.com>
2023-03-28 22:12:10 -0500

    Check for vertical and horizontal wins

--- ConnectFour.cs
+++ ConnectFour.cs
@@ -13,6 +13,8 @@ namespace Game
       {0,0,0,0,0,0,0}
     };
 
+    int _whoWon = 0;
+
     public int[,] Pieces()
     {
       return _pieces;
@@ -31,6 +33,7 @@ namespace Game
           i = -1;
         }
       }
+
       if(!piecePlaced)
         throw new Exception("Column full");
     }
@@ -38,6 +41,102 @@ namespace Game
     public void SetAllPieces(int[,] pieces)
     {
       _pieces = pieces;
+    }
+
+    public bool Won()
+    {
+      var who = LookForVirticalWin();
+      if(who > 0)
+      {
+        _whoWon = who;
+        return true;
+      }
+
+      who = LookForHorizontalWin();
+      if(who > 0)
+      {
+        _whoWon = who;
+        return true;
+      }
+
+      return false;
+    }
+
+    public int WhoWon()
+    {
+      return _whoWon;
+    }
+
+    int LookForVirticalWin()
+    {
+      int one;
+      int two;
+      for (int column = 0; column < 7; column++)
+      {
+        one = 0;
+        two = 0;
+        Console.WriteLine("Reset");
+        for (int row = 0; row < 6; row++)
+        {
+          if (_pieces[row, column] == 1)
+          {
+            Console.WriteLine($"row:{row}, col:{column}");
+            one++;
+            two = 0;
+            if(one == 4)
+              return 1;
+          }
+          else if (_pieces[row, column] == 2)
+          {
+            two++;
+            one = 0;
+            if(two == 4)
+              return 2;
+          }
+          else
+          {
+            one = 0;
+            two = 0;
+          }
+        }
+      }
+      return 0;
+    }
+
+    int LookForHorizontalWin()
+    {
+      int one;
+      int two;
+      for (int row = 0; row < 6; row++)
+      {
+        one = 0;
+        two = 0;
+        Console.WriteLine("Reset");
+        for (int column = 0; column < 7; column++)
+        {
+          if (_pieces[row, column] == 1)
+          {
+            Console.WriteLine($"row:{row}, col:{column}");
+            one++;
+            two = 0;
+            if(one == 4)
+              return 1;
+          }
+          else if (_pieces[row, column] == 2)
+          {
+            two++;
+            one = 0;
+            if(two == 4)
+              return 2;
+          }
+          else
+          {
+            one = 0;
+            two = 0;
+          }
+        }
+      }
+      return 0;
     }
   }
 }
--- ConnectFourTests.cs
+++ ConnectFourTests.cs
@@ -108,6 +108,53 @@ namespace Game
       Assert.Throws<Exception>(() => game.PlacePiece(1, 4));
     }
 
+    [Fact]
+    void new_game_is_not_won()
+    {
+      var game = new ConnectFour();
+
+      Assert.False(game.Won());
+      Assert.Equal(0, game.WhoWon());
+    }
+
+    [Fact]
+    void can_win_game_vitically()
+    {
+      var game = new ConnectFour();
+
+      int[,] pieces = new int[,] {
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0},
+        {0,0,0,0,1,0,0},
+        {0,2,0,0,1,0,0},
+        {2,2,0,0,1,0,0}
+      };
+      game.SetAllPieces(pieces);
+
+      Assert.True(game.Won());
+      Assert.Equal(1, game.WhoWon());
+    }
+
+    [Fact]
+    void can_win_game_horizontally()
+    {
+      var game = new ConnectFour();
+
+      int[,] pieces = new int[,] {
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0},
+        {0,0,0,0,1,0,0},
+        {0,1,0,0,1,1,0},
+        {0,2,1,2,2,2,2},
+        {2,2,2,1,1,0,0}
+      };
+      game.SetAllPieces(pieces);
+
+      Assert.True(game.Won());
+      Assert.Equal(2, game.WhoWon());
+    }
+
     void ConsoleDisplayPieces(int[,] pieces)
     {
       var board = new Board();
